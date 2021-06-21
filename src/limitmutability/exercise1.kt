c0bbc73149cb5d7aefebafebe7c395243d383cb6
Caique Minhare <caique.minhare@xpi.com.br>
2021-06-20 22:30:50 -0300

    Altered:  - Updating

---
+++ src/limitmutability/exercise1.kt
@@ -0,0 +1,32 @@
+package limitmutability
+
+import java.lang.Exception
+
+class InsufficientFunds: Exception()
+
+class BankAccount {
+    var balance = 0.0
+        private set
+
+    fun deposit(depositAmount: Double) {
+        balance += depositAmount
+    }
+
+    @Throws
+    fun withdraw(withdrawAmount: Double) {
+        if(balance < withdrawAmount) {
+            throw InsufficientFunds()
+        } else {
+            balance -= withdrawAmount
+        }
+    }
+}
+
+fun main() {
+    val account = BankAccount()
+    println(account.balance)
+    account.deposit(100.0)
+    println(account.balance)
+    account.withdraw(50.0)
+    println(account.balance)
+}
